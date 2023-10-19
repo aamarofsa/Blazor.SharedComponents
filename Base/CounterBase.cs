66c737c125aa2067cbf7a73a009f9f6271f8abf9
Alexis Amaro Placeres <45603252+terranigma22@users.noreply.github.com>
2023-10-19 09:50:03 -0400

    Separate Code Behind into Project Library

---
+++ Base/CounterBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Base
+{
+    public class CounterBase : ComponentBase
+    {
+        public int currentCount { get; set; } = 0;
+
+        public void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
